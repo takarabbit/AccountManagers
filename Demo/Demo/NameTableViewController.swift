dc935adf19e1600f8c8e5e4ff58087de4d245263
miyaoka <miyaoka@fenrir.co.jp>
2016-10-17 15:12:50 +0900

    コミット

--- Demo/Demo/NameTableViewController.swift
+++ Demo/Demo/NameTableViewController.swift
@@ -47,6 +47,11 @@ class NameTableViewController: UITableViewController {
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let nvc = navigationController {
+            nvc.popViewController(animated: true)
+        }
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
